ee0e7c25e94adf2daf938faf517f6f9484279dba
Alessio Treglia <alessio@tendermint.com>
2018-11-06 14:10:16 +0000

    Clean up

--- cmd/gaia/init/genesis_accts.go
+++ cmd/gaia/init/genesis_accts.go
@@ -2,7 +2,6 @@ package init
 
 import (
 	"fmt"
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/cmd/gaia/app"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/server"
@@ -14,11 +13,10 @@ import (
 	"github.com/tendermint/tendermint/libs/common"
 )
 
-// get cmd to initialize all files for tendermint and application
-// nolint
+// AddGenesisAccountCmd returns add-genesis-account cobra Command
 func AddGenesisAccountCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "add-genesis-account",
+		Use:   "add-genesis-account [address] [coin][,[coin]]",
 		Short: "Add genesis account to genesis.json",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(_ *cobra.Command, args []string) error {
@@ -63,8 +61,5 @@ func AddGenesisAccountCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command 
 	}
 
 	cmd.Flags().String(cli.HomeFlag, app.DefaultNodeHome, "node's home directory")
-	cmd.Flags().BoolP(flagOverwrite, "o", false, "overwrite the genesis.json file")
-	cmd.Flags().String(client.FlagChainID, "", "genesis file chain-id, if left blank will be randomly created")
-	cmd.Flags().String(flagMoniker, "", "set the validator's moniker")
 	return cmd
 }
